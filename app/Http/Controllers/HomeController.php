4af21ea3e47ca1775639e0629961206fcc82852c
Abigail Vargas Argüello <abivargasargu17@gmail.com>
2026-03-17 01:55:37 -0500

    Landing Page en el host principal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __invoke()
+    public function index()
     {
-        return view('welcome');
+    $featured = Product::with('category')->limit(8)->get();
+    $deals = Product::with('category')->orderBy('price')->limit(6)->get();
+    $totalProducts = Product::count(); // ← Conteo aquí
+    
+    return view('welcome', compact('featured', 'deals', 'totalProducts'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,13 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::prefix('/product')-> controller(ProductController::class) -> group(function(){
-
+Route::prefix('product')->controller(ProductController::class)->group(function(){
     Route::get('/', 'index')->name('product.index');
     Route::get('/create', 'create')->name('product.create');
     Route::post('/store', 'store')->name('product.store');
-    Route::get('/{id}', 'show');
+    Route::get('/{id}', 'show')->name('product.show');
     Route::delete('/{product}', 'destroy')->name('product.destroy');
 
 });
